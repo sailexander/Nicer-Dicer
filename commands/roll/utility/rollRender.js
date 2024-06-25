8916c6e98d82f16066c3e3452d8611b54297ea1e
sailexander <github@sailex.de>
2024-06-25 16:30:34 +0200

    Render bonus value and total.

--- commands/roll/utility/rollRender.js
+++ commands/roll/utility/rollRender.js
@@ -10,8 +10,8 @@ const COLOR_DICE = 'white';
 module.exports = { rollRender }
 
 async function rollRender(diceRoll) {
-    const canvasWidth = diceRoll.diceAmount * (DICE_SIZE + MARGIN) + MARGIN;
-    const canvasHeight = DICE_SIZE + 2 * MARGIN;
+    const canvasWidth = diceRoll.diceAmount * (DICE_SIZE + MARGIN) + MARGIN + (diceRoll.bonusValue > 0 ? DICE_SIZE + MARGIN : 0);
+    const canvasHeight = 2 * DICE_SIZE + 3 * MARGIN;
     const canvas = createCanvas(canvasWidth, canvasHeight);
     const context = canvas.getContext('2d');
 
@@ -25,15 +25,31 @@ async function rollRender(diceRoll) {
 
     for (let i in diceRoll.rolls) {
         renderDice(
-            context, 
-            diceShapes[diceRoll.diceType], 
+            context,
+            diceShapes[diceRoll.diceType] ?? diceShapes['W10'],
             diceRoll.rolls[i],
             MARGIN + i * (DICE_SIZE + MARGIN),
             MARGIN
         );
     }
 
-    return new AttachmentBuilder(await canvas.encode('png'), { name: 'roll.png'});
+    if (diceRoll.bonusValue > 0) {
+        renderBonusValue(
+            context,
+            diceRoll.bonusValue,
+            MARGIN + diceRoll.diceAmount * (DICE_SIZE + MARGIN),
+            MARGIN
+        );
+    }
+
+    renderTotal( 
+        context,
+        diceRoll.total,
+        canvasWidth,
+        DICE_SIZE + 2 * MARGIN
+    );
+
+    return new AttachmentBuilder(await canvas.encode('png'), { name: 'roll.png' });
 }
 
 async function renderDice(context, shape, number, x, y, color = '#ffffff') {
@@ -52,6 +68,31 @@ async function renderDice(context, shape, number, x, y, color = '#ffffff') {
     context.strokeRect(0, 0, 50, 50);
     context.strokeRect(0, 0, 25, 25);
     context.strokeRect(25, 25, 25, 25); */
+}
 
-    return context;
+async function renderBonusValue(context, number, x, y, color = '#ffffff') {
+    context.setTransform(1, 0, 0, 1, 0, 0);
+
+    context.fillStyle = color;
+    context.translate(x, y);
+
+    context.font = '20px sans-serif';
+    let textMeasure = context.measureText(`+ ${number.toString()}`);
+    context.fillText(`+ ${number.toString()}`, 25 - (textMeasure.width / 2), 33);
+
+    /* context.strokeStyle = 'red';
+    context.strokeRect(0, 0, 50, 50);
+    context.strokeRect(0, 0, 25, 25);
+    context.strokeRect(25, 25, 25, 25); */
+}
+
+async function renderTotal(context, number, canvasWidth, y, color = '#ffffff') {
+    context.setTransform(1, 0, 0, 1, 0, 0);
+
+    context.fillStyle = color;
+    context.translate(0, y);
+
+    context.font = '40px sans-serif';
+    let textMeasure = context.measureText(number.toString());
+    context.fillText(number.toString(), canvasWidth / 2 - (textMeasure.width / 2), 33);
 }
